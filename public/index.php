cd4f6677a64019b8ccfe83d8db6290f83d5ee8aa
Thomas Moraes <tho.vini7@gmail.com>
2023-04-30 11:27:57 -0300

    delete notes route

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use CleanArchitecture\Application\Factories\Auth\MakeSigIn;
 use CleanArchitecture\Application\Factories\Auth\MakeSignUp;
 use CleanArchitecture\Application\Factories\Note\MakeCreateNote;
+use CleanArchitecture\Application\Factories\Note\MakeDeleteNote;
 use CleanArchitecture\Application\Factories\Note\MakeLoadNote;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
@@ -27,6 +28,7 @@ $app->post("/signup", MakeSignUp::class);
 $app->group("/notes", function(RouteCollectorProxy $group) {
     $group->post("", MakeCreateNote::class);
     $group->get("", MakeLoadNote::class);
+    $group->delete("/{id}", MakeDeleteNote::class);
 });
 
 $app->run();
---
+++ src/Application/Factories/Note/MakeDeleteNote.php
@@ -0,0 +1,37 @@
+<?php 
+namespace CleanArchitecture\Application\Factories\Note;
+
+use DI\Container;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use CleanArchitecture\Application\UseCases\Note\DeleteNote;
+use CleanArchitecture\Application\Controllers\ControllerOperation;
+use CleanArchitecture\Application\Controllers\Note\DeleteNoteOperation;
+
+class MakeDeleteNote
+{
+    private ControllerOperation $controller;
+
+    public function __construct(Container $container)
+    {
+        $noteRepository = $container->get("NoteRepository");
+
+        $useCase = new DeleteNote($noteRepository);
+        $this->controller = new DeleteNoteOperation($useCase);
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $payload["id"] = $args["id"];
+        $responseController = $this->controller->handle($payload);
+
+        $response->getBody()->write(json_encode($responseController));
+
+        return $response->withHeader('Content-Type', 'application/json')
+        ->withHeader("Cache-Control", "no-cache")
+        ->withHeader("Cache-Control", "max-age=0")
+        ->withHeader("Cache-Control", "must-revalidate")
+        ->withHeader("Cache-Control", "proxy-revalidate")
+        ->withStatus($responseController['statusCode']);
+    }
+}
